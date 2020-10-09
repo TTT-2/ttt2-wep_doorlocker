0de8412b9b6d8716ab7920c8796bacdf56587b07
ThePlatinumGhost <36534723+ThePlatinumGhost@users.noreply.github.com>
2020-10-09 18:16:45 +0200

    Create italian

---
+++ lua/terrortown/lang/italian/doorlocker.lua
@@ -0,0 +1,13 @@
+L = LANG.GetLanguageTableReference("italiano")
+
+L["weapon_doorlocker_name"] = "Blocca Porte"
+L["weapon_doorlocker_desc"] = "Blocca le porte per nasconderti. Ma stai attento che le porte chiuse possono essere distrutte."
+L["door_not_lockable"] = "Questa porta non può essere bloccata."
+L["door_already_locked"] = "Questa porta è gia bloccata."
+L["door_is_open"] = "Questa porta non può essere bloccata perchè è aperta."
+L["door_not_locked"] = "Questa porta non può essere sbloccata perchè non è bloccata ."
+L["door_locked_not_you"] = "Questa porta non può essere sbloccata perchè non è bloccata da te."
+L["door_now_locked"] = "Bloccato correttamente la porta."
+L["door_now_unlocked"] = "Sbloccato correttamente la porta."
+L["door_help_msb1"] = "Blocca la porta"
+L["door_help_msb2"] = "Sblocca la porta"
