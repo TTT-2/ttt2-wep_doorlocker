992757953c4069f01081556ceefbdaebecda887a
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-11-03 09:31:47 +0100

    French translation (#4)

---
+++ lua/terrortown/lang/french/doorlocker.lua
@@ -0,0 +1,13 @@
+L = LANG.GetLanguageTableReference("french")
+
+L["weapon_doorlocker_name"] = "Verrouilleur de porte"
+L["weapon_doorlocker_desc"] = "Verrouillez les portes pour vous cacher dans les zones sécurisées. Mais attention, les portes verrouillées deviennent destructibles."
+L["door_not_lockable"] = "Cette porte ne peut pas être verrouillée."
+L["door_already_locked"] = "Cette porte est déjà verrouillée."
+L["door_is_open"] = "Cette porte ne peut pas être verrouillée car elle est ouverte."
+L["door_not_locked"] = "Cette porte ne peut pas être déverrouillée car elle n'est pas verrouillée."
+L["door_locked_not_you"] = "Cette porte ne peut pas être déverrouillée parce que elle na pas été verrouillée par vous."
+L["door_now_locked"] = " Porte verrouillée avec succès."
+L["door_now_unlocked"] = "Porte déverouillée avec succès."
+L["door_help_msb1"] = "Verrouille la porte"
+L["door_help_msb2"] = "Déverrouille la porte"
