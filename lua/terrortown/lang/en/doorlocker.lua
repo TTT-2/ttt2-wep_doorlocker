23a63deb9c3893a52f281ea3c8a02b5cf8a11eef
uenetron <srb5467@gmail.com>
2021-11-15 11:42:53 +0000

    Small language fixes (#5)

--- lua/terrortown/lang/en/doorlocker.lua
+++ lua/terrortown/lang/en/doorlocker.lua
@@ -1,12 +1,12 @@
 L = LANG.GetLanguageTableReference("en")
 
 L["weapon_doorlocker_name"] = "Doorlocker"
-L["weapon_doorlocker_desc"] = "Lock doors to hideout in secreat areas. But beware that locked doors become destructible."
-L["door_not_lockable"] = "This door can not be locked."
+L["weapon_doorlocker_desc"] = "Lock doors to hide out in secret areas. Beware that locked doors become destructible."
+L["door_not_lockable"] = "This door cannot be locked."
 L["door_already_locked"] = "This door is already locked."
 L["door_is_open"] = "This door cannot be locked because it is currently open."
 L["door_not_locked"] = "This door cannot be unlocked because it is not locked."
-L["door_locked_not_you"] = "This door cannot be unlocked because it is not locked by you."
+L["door_locked_not_you"] = "This door cannot be unlocked because it was not locked by you."
 L["door_now_locked"] = "Successfully locked the door."
 L["door_now_unlocked"] = "Successfully unlocked the door."
 L["door_help_msb1"] = "Locks the door"
